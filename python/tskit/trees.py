61c95423c83f180e9c66536407100804d5a64a8f
Yan Wong <yan.wong@bdi.ox.ac.uk>
2020-08-19 21:29:19 +0100

    Document edge order in edge_diffs
    It's useful to know if e.g. we can group by parent id in the edges_in and edges_out lists (which are, indeed, lists, not tuples, as documented)

--- python/tskit/trees.py
+++ python/tskit/trees.py
@@ -3446,14 +3446,21 @@ class TreeSequence:
         The iterator yields a sequence of 3-tuples, ``(interval, edges_out,
         edges_in)``. The ``interval`` is a pair ``(left, right)`` representing
         the genomic interval (see :attr:`Tree.interval`). The ``edges_out``
-        value is a tuple of the edges that were just-removed to create the tree
+        value is a list of the edges that were just-removed to create the tree
         covering the interval (hence ``edges_out`` will always be empty for the
-        first tree). The ``edges_in`` value is a tuple of edges that were just
+        first tree). The ``edges_in`` value is a list of edges that were just
         inserted to construct the tree convering the current interval. The
-        iterator terminates after the final interval in the tree sequence (i.e.
-        it does not report a final removal of all remaining edges); the number
-        of iterations is thus always equal to the number of trees in the tree
-        sequence.
+        edges in each ``edges_in`` list are returned in the
+        :ref:`order required for a valid tree sequence <sec_edge_requirements>`
+        (e.g. edges appear in order of ascending parent node id). The edges in
+        each ``edges_out`` list are the reverse order (e.g. edges appear
+        in order of descending parent node id).
+        
+        
+        This iterator terminates after the final interval in the tree sequence
+        (i.e. it does not report a final removal of all remaining edges); the
+        number of iterations is thus always equal to the number of trees in the
+        tree sequence.
 
         :return: An iterator over the (interval, edges_out, edges_in) tuples.
         :rtype: :class:`collections.abc.Iterable`
